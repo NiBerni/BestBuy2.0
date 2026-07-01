8cd95d77e01893b2d83456cab8ed9c836f1a7862
Berni <277801407+NiBerni@users.noreply.github.com>
2026-07-01 07:24:47 +0200

    test(store): Relocate store tests
    *   Move `Store` class tests from `test_product.py` to `test_store.py`.

--- tests/test_product.py
+++ tests/test_product.py
@@ -3,7 +3,6 @@ import pytest
 
 import promotions
 from products import LimitedProduct, NonStockedProduct, Product
-from store import Store
 
 
 def test_create_normal_product() -> None:
@@ -112,44 +111,3 @@ def test_product_str() -> None:
     discount = promotions.PercentDiscount("20% off", percent=20)
     product.set_promotion(discount)
     assert str(product) == "Test Product, Price: 100, Quantity: 10, Promotion: 20% off"
-
-
-def test_store_add_remove_product() -> None:
-
-    p1 = Product("Test 1", price=10, quantity=10)
-    p2 = Product("Test 2", price=20, quantity=20)
-    store = Store([p1])
-    store.add_product(p2)
-    assert len(store.products) == 2
-    store.remove_product(p1)
-    assert len(store.products) == 1
-    assert store.products[0].name == "Test 2"
-
-
-def test_store_get_total_quantity() -> None:
-
-    p1 = Product("Test 1", price=10, quantity=10)
-    p2 = Product("Test 2", price=20, quantity=20)
-    store = Store([p1, p2])
-    assert store.get_total_quantity() == 30
-
-
-def test_store_get_all_products() -> None:
-
-    p1 = Product("Test 1", price=10, quantity=10)
-    p2 = Product("Test 2", price=20, quantity=0)
-    store = Store([p1, p2])
-    active_products = store.get_all_products()
-    assert len(active_products) == 1
-    assert active_products[0].name == "Test 1"
-
-
-def test_store_order() -> None:
-
-    p1 = Product("Test 1", price=10, quantity=10)
-    p2 = Product("Test 2", price=20, quantity=20)
-    store = Store([p1, p2])
-    shopping_list = [(p1, 2), (p2, 1)]
-    assert store.order(shopping_list) == 40.0
-    assert p1.quantity == 8
-    assert p2.quantity == 19
